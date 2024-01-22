61425900659fc63454d12f9a815ba939a036247e
Mariama1001 <154673023+Mariama1001@users.noreply.github.com>
2024-01-22 14:14:56 +0100

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-import multiprocessing
 import Threading
+import multiprocessing
+from multiprocessing import Lock
 
 # Constants
-NUMBER_OF_PLAYERS = 3
+MIN_PLAYERS = 2
+MAX_PLAYERS = 5
+
+# Get the number of players from the user
+while True:
+    try:
+        num_players = int(input(f"Enter the number of players (between {MIN_PLAYERS} and {MAX_PLAYERS}): "))
+        if MIN_PLAYERS <= num_players <= MAX_PLAYERS:
+            break
+        else:
+            print(f"Please enter a number between {MIN_PLAYERS} and {MAX_PLAYERS}.")
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+
+NUMBER_OF_PLAYERS = num_players
 NUMBER_OF_COLORS = NUMBER_OF_PLAYERS
 
 # Shared memory
@@ -14,5 +29,6 @@ Mutex = Lock()
 # Other global variables
 game_over = False
 current_player = 0
+
 
 def game_logic():
