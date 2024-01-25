1bea8705047a6db318cafe0eac9fa7a154a9877c
Mariama1001 <154673023+Mariama1001@users.noreply.github.com>
2024-01-25 09:35:24 +0100

    game.py

--- game.py
+++ game.py
@@ -1,1 +1,33 @@
+import multiprocessing
+import socket
+import pickle
+
+def game_process(num_players, player_conn):
+    # Initialisation du jeu
+    # ...
+
+    try:
+        while True:
+            # Envoi d'informations aux joueurs
+            game_info = {'player_hands': [...], 'other_game_state': ...}
+            player_conn.send(pickle.dumps(game_info))
+
+            # Réception des actions des joueurs
+            player_actions = []
+            for _ in range(num_players):
+                action = pickle.loads(player_conn.recv())
+                player_actions.append(action)
+
+            # Mise à jour de l'état du jeu en fonction des actions des joueurs
+            # ...
+
+            # Vérification des conditions de fin du jeu
+            if game_over:
+                break
+
+    except KeyboardInterrupt:
+        print("Game process interrupted.")
+    finally:
+        # Nettoyage et gestion de la fin du jeu
+        # ...
 
